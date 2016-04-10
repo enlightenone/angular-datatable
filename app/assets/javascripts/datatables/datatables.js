660e03200ad578ccedc57518736aabdc6dcfe14d
enlightenone <stevenwchow@gmail.com>
2016-04-10 16:31:00 -0700

    Show/Hide columns function is finished

--- app/assets/javascripts/datatables/datatables.js
+++ app/assets/javascripts/datatables/datatables.js
@@ -1,7 +1,31 @@
 $(document).ready(function() {
-    $('#example').DataTable({
+    var table = $('#example').DataTable({
         colReorder: true,
         stateSave: true,
         "scrollX": true
     });
-} );
+
+
+    $("input.toggle-visit").each(function(){
+      var $input = $(this);
+      var col_number = $input.attr('data-column-num');
+      var col = table.column(col_number);
+      var visibility = col.visible();
+
+
+      if (!visibility){
+         $input.prop("checked", false);
+        }
+    });
+
+
+    $("input.toggle-visit").change(function(){
+       var column_number = $(this).attr('data-column-num');
+
+       var column = table.column(column_number);
+        console.log("Column Visibility");
+        console.log(column.visible());
+
+        column.visible(!column.visible());
+    });
+});
